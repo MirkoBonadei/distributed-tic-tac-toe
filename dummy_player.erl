d1b98b328f04e1e0a527afe93ced62bb586d4944
Mirko Bonadei <mirko.bonadei@gmail.com>
2014-07-26 10:23:08 +0200

    Implemented a dummy_player.

---
+++ dummy_player.erl
@@ -0,0 +1,49 @@
+-module(dummy_player).
+-export([start/1, stop/1, next_move/2]).
+-export([init/1]).
+
+-include_lib("eunit/include/eunit.hrl").
+
+% public functions
+
+start(Name) ->
+  spawn(?MODULE, init, [Name]).
+
+init(Name) ->
+  loop(Name).
+
+stop(PlayerPid) ->
+  call(PlayerPid, stop).
+
+next_move(PlayerPid, AvailableMoves) ->
+  call(PlayerPid, {next_move, AvailableMoves}).
+
+% private functions
+
+loop(Name) ->
+  receive
+    {request, Pid, stop} ->
+      io:format("Player ~p is going down~n", [Name]),
+      reply(Pid, ok);
+    {request, Pid, {next_move, AvailableMoves}} ->
+      Move = dummy_move(AvailableMoves),
+      io:format("Player ~p is choosing ~p~n", [Name, Move]),
+      reply(Pid, Move)
+  end.
+
+call(Pid, Msg) ->
+  Pid ! {request, self(), Msg},
+  receive
+    {reply, Reply} -> Reply
+  end.
+
+reply(Pid, Reply) ->
+  Pid ! {reply, Reply}.
+
+dummy_move([FirstAvailablePosition|_RestOfAvailablePositions]) ->
+  FirstAvailablePosition.
+
+
+-ifdef(TEST).
+
+-endif.
