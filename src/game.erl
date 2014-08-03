aad79b4172437555c616d6d8655f136c4454be36
Mirko Bonadei <mirko.bonadei@gmail.com>
2014-08-03 19:21:13 +0200

    Finished to implement a basic tic-tac-toe.
    I am not satisfied by the testability of the system.
    And I have also to implement the monitoring of the players with the and
    the scenario when the player leaves the game.

--- src/game.erl
+++ src/game.erl
@@ -7,11 +7,19 @@
          init/1,
          handle_cast/2,
          handle_call/3,
+         handle_info/2,
          terminate/2
         ]).
 -behaviour(gen_server).
 
 -define(TEST_TIMEOUT, 1000).
+
+-define(TICK_TIME, 2000).
+-define(TICK_MESSAGE, "tick").
+
+-define(WAITING_FOR_PLAYERS, "waiting_for_players").
+-define(ONGOING, "ongoing").
+-define(TERMINATED, "terminated").
 
 %%% Game API
 
@@ -27,24 +35,41 @@ join(GamePid, PlayerPid) ->
 %%% gen_server callbacks
 
 init([]) ->
-  {ok, [[], board:new()]}.
+  {ok, [?WAITING_FOR_PLAYERS, [], board:new()]}.
 
 handle_cast(stop, State) ->
   {stop, normal, State}.
 
-handle_call({join, PlayerPid}, _From, [[], Board]) ->
-  {reply, {ok, welcome}, [[PlayerPid], Board]}.
+handle_call({join, PlayerPid}, _From, [?WAITING_FOR_PLAYERS, [], Board]) ->
+  {reply, {ok, welcome}, [?WAITING_FOR_PLAYERS, [PlayerPid], Board]};
+handle_call({join, PlayerPid}, _From, [?WAITING_FOR_PLAYERS, [PlayerOne], Board]) ->
+  Timer = erlang:send_after(?TICK_TIME, self(), ?TICK_MESSAGE),
+  {reply, {ok, welcome}, [?ONGOING, [PlayerOne, PlayerPid], Board, Timer]}.
 
-terminate(normal, State) ->
-  io:format(
-    "game has been destroyed with state ~p~n",
-    [State]
-  );
-terminate(Reason, State) ->
-  io:format(
-    "game has been terminated with reason ~p and with state~p~n",
-    [Reason, State]
-  ).
+handle_info(?TICK_MESSAGE, [?ONGOING, [NextPlayer, PausedPlayer], Board, OldTimer]) ->
+  erlang:cancel_timer(OldTimer),
+  {X, Y} = player:next_move(NextPlayer, Board),
+  NewBoard = board:make_move(Board, NextPlayer, X, Y),
+  io:format("~p~n", [NewBoard]),
+  case board:has_been_won_by(NewBoard, NextPlayer) of
+    true ->
+      player:winner(NextPlayer),
+      player:loser(PausedPlayer),
+      {stop, normal, [?TERMINATED, [PausedPlayer, NextPlayer], NewBoard]};
+    false ->
+      case board:available_positions(NewBoard) of
+        [] ->
+          player:draw(NextPlayer),
+          player:draw(PausedPlayer),
+          {stop, normal, [?TERMINATED, [PausedPlayer, NextPlayer], NewBoard]};
+        _ ->
+          NewTimer = erlang:send_after(?TICK_TIME, self(), ?TICK_MESSAGE),
+          {noreply, [?ONGOING, [PausedPlayer, NextPlayer], NewBoard, NewTimer]}
+      end
+  end.
+
+terminate(_Reason, _State) ->
+  ok.
 
 -ifdef(TEST).
 
@@ -57,12 +82,22 @@ game_should_be_created_and_destroyed_test() ->
      fun() -> game:destroy(GamePid) end
   ).
 
-game_should_accept_join_request_from_player_test_() ->
+game_should_accept_join_request_from_players_test_() ->
   {setup,
-   fun() -> {ok, GamePid} = game:create(), GamePid end,
-   fun(GamePid) -> game:destroy(GamePid) end,
-   fun(GamePid) ->
-       [?_assertMatch({ok, welcome}, game:join(GamePid, self()))]
+   fun() ->
+       {ok, Game} = game:create(),
+       {ok, PlayerOne} = player:create("player one"),
+       {ok, PlayerTwo} = player:create("player two"),
+       [Game, PlayerOne, PlayerTwo]
+   end,
+   fun([Game, PlayerOne, PlayerTwo]) ->
+       game:destroy(Game),
+       player:destroy(PlayerOne),
+       player:destroy(PlayerTwo)
+   end,
+   fun([Game, PlayerOne, PlayerTwo]) ->
+       [?_assertMatch({ok, welcome}, game:join(Game, PlayerOne)),
+        ?_assertMatch({ok, welcome}, game:join(Game, PlayerTwo))]
    end
   }.
 
--- src/player.erl
+++ src/player.erl
@@ -2,10 +2,19 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("include/testing_macros.hrl").
 
--export([create/1, destroy/1]).
+-export([
+         create/1,
+         destroy/1,
+         join/2,
+         next_move/2,
+         winner/1,
+         loser/1,
+         draw/1
+        ]).
 -export([
          init/1,
          handle_cast/2,
+         handle_call/3,
          terminate/2
         ]).
 -behaviour(gen_server).
@@ -20,24 +29,51 @@ create(Name) ->
 destroy(PlayerPid) ->
   gen_server:cast(PlayerPid, stop).
 
+join(PlayerPid, GamePid) ->
+  game:join(GamePid, PlayerPid).
+
+next_move(PlayerPid, Board) ->
+  gen_server:call(PlayerPid, {next_move, Board}).
+
+winner(PlayerPid) ->
+  gen_server:cast(PlayerPid, winner).
+
+loser(PlayerPid) ->
+  gen_server:cast(PlayerPid, loser).
+
+draw(PlayerPid) ->
+  gen_server:cast(PlayerPid, draw).
+
 %%% gen_server callbacks
 
 init([Name]) ->
   {ok, Name}.
 
 handle_cast(stop, Name) ->
-  {stop, normal, Name}.
+  {stop, normal, Name};
+handle_cast(winner, Name) ->
+  io:format("~p: I am the winner~n", [self()]),
+  {noreply, Name};
+handle_cast(loser, Name) ->
+  io:format("~p: I am the loser~n", [self()]),
+  {noreply, Name};
+handle_cast(draw, Name) ->
+  io:format("~p: I am not a winner and not a loser~n", [self()]),
+  {noreply, Name}.
 
-terminate(normal, Name) ->
-  io:format(
-    "player ~p has been destroyed~n",
-    [Name]
-  );
-terminate(Reason, Name) ->
-  io:format(
-    "player ~p has been terminated with reason ~p~n",
-    [Name, Reason]
-  ).
+handle_call({next_move, Board}, _From, Name) ->
+  AvailablePositions = board:available_positions(Board),
+  {reply, random_move(AvailablePositions), Name}.
+
+terminate(_Reason, _Name) ->
+  ok.
+
+%%% private functions
+
+random_move(AvailableMoves) ->
+  <<A1, A2, A3>> = crypto:strong_rand_bytes(3),
+  random:seed(A1, A2, A3),
+  lists:nth(random:uniform(length(AvailableMoves)), AvailableMoves).
 
 -ifdef(TEST).
 
