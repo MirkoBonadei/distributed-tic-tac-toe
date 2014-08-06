187e126dfc890660e3fd904e6a4dc16dd1e49283
Mirko Bonadei <mirko.bonadei@gmail.com>
2014-08-06 19:13:42 +0200

    rebar lib skeleton

---
+++ src/tic_tac_toe.erl
@@ -0,0 +1,18 @@
+-module(tic_tac_toe).
+
+%% tic_tac_toe: tic_tac_toe library's entry point.
+
+-export([my_func/0]).
+
+
+%% API
+
+my_func() ->
+    ok().
+
+%% Internals
+
+ok() ->
+    ok.
+
+%% End of Module.
