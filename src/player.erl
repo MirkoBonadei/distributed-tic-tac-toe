483e91293f84a0cd77cbb039774f0663b7237d14
Mirko Bonadei <mirko.bonadei@gmail.com>
2014-08-12 00:19:12 +0200

    Refactored player test with the use of meck and fixtures.

--- src/player.erl
+++ src/player.erl
@@ -86,46 +86,27 @@ player_should_be_created_and_destroyed_test() ->
      fun() -> player:destroy(PlayerPid) end
   ).
 
-player_should_ask_game_to_join_test() ->
-    {ok, GamePid} = game:create(),
-    {ok, PlayerPid} = player:create("luke"),
-
-    ok = meck:new(game, [passthrough]),
-    meck:expect(
-      game,
-      join,
-      fun(PidOne, PidTwo) when is_pid(PidOne) and is_pid(PidTwo) ->
-          {ok, welcome};
-         (_, _) -> error(wrong_parameter_type)
-      end
-    ),
-
-    player:join(PlayerPid, GamePid),
-
-    ?assert(meck:called(game, join, [GamePid, PlayerPid])),
-    ?assert(meck:validate(game)),
-
-    ok = meck:unload(game),
-    game:destroy(GamePid),
-    player:destroy(PlayerPid).
-  % {setup,
-  %  fun() ->
-  %      {ok, GamePid} = game:create(),
-  %      {ok, PlayerPid} = player:create("luke"),
-  %      ok = meck:new(game, [passthrough]),
-  %      [GamePid, PlayerPid]
-  %   end,
-  %  fun([GamePid, PlayerPid]) ->
-  %      ok = meck:unload(game),
-  %      game:destroy(GamePid),
-  %      player:destroy(PlayerPid)
-  %  end,
-  %  fun([GamePid, PlayerPid]) ->
-  %      meck:expect(game, join, fun(_, _) -> {ok, welcome} end),
-  %      player:join(PlayerPid, GamePid),
-  %      ?_assert(meck:called(game, join, [GamePid, PlayerPid])),
-  %      ?_assert(meck:validate(game))
-  %  end
-  % }.
+player_should_ask_game_to_join_test_() ->
+  {setup,
+   fun() ->
+       {ok, PlayerPid} = player:create("luke"),
+       ok = meck:new(game, [passthrough]),
+       {ok, GamePid} = game:create(),
+       [GamePid, PlayerPid]
+    end,
+   fun([GamePid, PlayerPid]) ->
+       ok = meck:unload(game),
+       game:destroy(GamePid),
+       player:destroy(PlayerPid)
+   end,
+   fun([GamePid, PlayerPid]) ->
+      meck:expect(game, join, fun(P1, P2) when is_pid(P1) and is_pid(P2) -> {ok, welcome} end),
+      player:join(PlayerPid, GamePid),
+      [
+          ?_assert(meck:called(game, join, [GamePid, PlayerPid])),
+          ?_assert(meck:validate(game))
+      ]
+   end
+  }.
 
 -endif.
